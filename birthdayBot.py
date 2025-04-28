e81cd5373f11c8dbbd62be9083ad30890309e612
ImCloudy <Liamcoffey24@gmail.com>
2025-04-28 10:48:41 +1000

    updated code to work for MAC db, still need to filter some more then can combine

--- birthdayBot.py
+++ birthdayBot.py
@@ -1,9 +1,10 @@
 import requests
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 
-date = "2025-01-05"
-
+today = datetime.now()
+date = today.strftime("%m-%d")  #date in MM-DD format because birthdays are stored with different years
 load_dotenv()
 
 NOTION_TOKEN = os.getenv('NOTION_TOKEN')
@@ -15,17 +16,16 @@ headers = {
     "Notion-Version": "2022-06-28",
 }
 
-def get_pages(num_pages=None):
+def get_pages():
     url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
 
-    get_all = num_pages is None
-    page_size = 100 if get_all else num_pages
+    get_all = True   #ensures all data is collected 
+    page_size = 150  #will need to be increased if no. MAC members exceeds 150
 
     payload = {"page_size": page_size}
     response = requests.post(url, json=payload, headers=headers)
 
     data = response.json()
-
     results = data["results"]
     while data["has_more"] and get_all:
         payload = {"page_size": page_size, "start_cursor": data["next_cursor"]}
@@ -38,8 +38,9 @@ def get_pages(num_pages=None):
 
 pages = get_pages()
 for page in pages:
-    if page["properties"]["Date"]["date"]["start"] == date:
-        print(page["properties"]["Name"])
-        
-
+    bday = page["properties"]["Birthday"]["date"]
+    if bday == None:
+        continue
+    elif bday["start"][-5:] == date:
+        print(page["properties"]["Name"]["title"][0]["text"]["content"])
     
